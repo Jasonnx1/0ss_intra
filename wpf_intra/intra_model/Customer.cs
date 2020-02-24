4b766f3f4ed9ac8b090601400db86bb8b839db2c
Jason Gelinas <1742395@cshawi.ca>
2020-02-24 13:54:12 -0500

    Menu_StatusBar

---
+++ wpf_intra/intra_model/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace intra_model
+{
+    public class Customer : INotifyPropertyChanged
+    {
+
+
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+
+
+        private string name;
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Info");
+            }
+        }
+
+        public string Info => $"{Name}";
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ wpf_intra/wpf_intra/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace wpf_intra
+{
+    class Customer : INotifyPropertyChanged
+    {
+        
+
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+
+
+        private string name;
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                Name = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Info");
+            }
+        }
+
+        public string Info => $"{Name}";
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
